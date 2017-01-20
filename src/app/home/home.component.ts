f1cf6955c397a26c556cf772c603a9e762291fac
myrNG <my.ni.jp@gmail.com>
2017-01-20 12:28:44 +0100

    ras

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {User} from "../models/user";
+import {Student} from "../models/student";
 import {AuthentificationService} from "../services/authentification.service";
 import {Router} from "@angular/router";
 
@@ -10,21 +11,23 @@ import {Router} from "@angular/router";
 })
 export class HomeComponent implements OnInit {
 	currentUser: User;
-	
+
 	constructor(private auth: AuthentificationService, private router: Router) {
 		this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
 	}
-	
+
 	ngOnInit() {
 		console.log('home detected -> ', this.currentUser)
 	}
-	
+
 	disconnectMe() {
 		console.log('Deconnexion en cours...');
 		this.auth.logOut();
 		this.router.navigate(['/login']);
 	}
-	
+
+
+
 }
 
 
