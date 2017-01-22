4c73d13ac7dbef0288de0c48c7804558cea90c04
myrNG <my.ni.jp@gmail.com>
2017-01-22 10:27:56 +0100

    final method with real data waiting

--- src/app/services/listing.service.ts
+++ src/app/services/listing.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from "@angular/http";
+import {Http, Response, RequestOptions, Request, RequestMethod} from "@angular/http";
 import 'rxjs/add/operator/map';
 
 import {Observable} from "rxjs";
@@ -10,48 +10,56 @@ import {Student} from "../models/student";
 @Injectable()
 export class ListingService {
   private listingUrl: string = 'https://jsonplaceholder.typicode.com/users';
+  //private listingUrl: string = 'dash-school.hol.es/api/web/listing';
+  //private studentUrl: string = 'dash-school.hol.es/api/web/listing/detailStudent';
 
   students: Observable<Student[]>;
 
   constructor(private http: Http) {
   }
 
-  getListing() {
+  /*getListing() {
     console.log('Getting Listing');
+
     return this.http.get(this.listingUrl)
       .map((res: Response) => <Student[]> res.json())
       .catch(this.handleError);
 
   }
+  */
+  getListing(){
+     // Permet de personnalister la requete avec l'id du student
+     //const id = new RequestOptions({ method: RequestMethod.Get });
+     return this.http.get(this.listingUrl) // rajouter interpo de id
+            .map((res: Response) => <Student[]> res.json())
+            .catch(this.handleError);
+   }
+  /*
+  * getListing(){
+  *   // Permet de personnalister la requete avec l'id du student
+  *   const id = new RequestOptions({ method: RequestMethod.Get });
+  *   return this.http.get(this.listingUrl, options) // rajouter interpo de id
+  *     .map(res: Response) => <Student[]> res.json()
+  *     .catch(this.handleError);
+  * }
+  *
+  * getStudent(id:number){
+  *   return this.http.get(`dash-school.hol.es/api/web/listing/detailStudent/${id}`)
+  *     .map((res: Response) => <Student> res.json())
+  *     .catch(this.handleError);
+  * }
+  * */
+
 
   handleError(error: Response) {
     console.error(error);
     return Observable.throw(error.json().error || 'Server Error');
   }
+  addStudent(){ }
+
+  editStudent(){ }
+
+  deleteStudent(){}
 }
 
-// Interface ou Class ?
-/*export interface Student {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string,
-    suite: string,
-    city: string,
-    zipcode: string,
-    geo: {
-      lat: string;
-      lng: string
-    }
-  };
-  phone: string;
-  website: string;
-  company: {
-    name: string,
-    catchPhrase: string,
-    bs: string
-  };
-}
-*/
+
