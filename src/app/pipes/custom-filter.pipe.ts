dea8fafa86c776df0a51377a54d35618b7d3abce
myrNG <my.ni.jp@gmail.com>
2017-01-24 10:02:09 +0100

    add custom pipe filter

--- src/app/pipes/custom-filter.pipe.ts
+++ src/app/pipes/custom-filter.pipe.ts
@@ -7,11 +7,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CustomFilterPipe implements PipeTransform {
 
   transform(students: any, value: any): any {
+    // Si rien n'est saisi, la liste complète des étudiants est affichée
     if(value === undefined){
       return students;
     }
+    /*
+    * on filtre les étudiants via la saisie dont la valeur de l'input passe automatiquement en minuscule
+    * Filtre via le lastname de l'élève
+    *
+    * */
+
     return students.filter(function(student) {
-      return student.lastname.toLocaleLowerCase().include(value.toLocaleLowerCase());
+      return student.lastname.toLowerCase()
+                    .includes(value.toLowerCase());
+
     })
 
   }
